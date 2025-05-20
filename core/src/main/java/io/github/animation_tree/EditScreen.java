4176b7884e45c9b5d20dc843d590db5dc765b2e0
Vannos-Slid <ivan.kush05@gmail.com>
2025-05-20 16:33:23 +0300

    added some drag options

--- core/src/main/java/io/github/animation_tree/EditScreen.java
+++ core/src/main/java/io/github/animation_tree/EditScreen.java
@@ -1,17 +1,23 @@
 package io.github.animation_tree;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.InputMultiplexer;
+import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import com.badlogic.gdx.scenes.scene2d.utils.DragListener;
 import com.badlogic.gdx.utils.ScreenUtils;
 import com.badlogic.gdx.utils.viewport.StretchViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 
-public class EditScreen implements Screen {
+public class EditScreen implements Screen, InputProcessor {
 
     //Screen
     private OrthographicCamera camera;
@@ -25,8 +31,13 @@ public class EditScreen implements Screen {
     private SpriteBatch batch;
     private Stage stage;
 
+//    InputMultiplexer multiplexer;
+
     private Texture texture;
+    private Texture texture2;
     private Sprite sprite;
+    private Sprite sprite2;
+
 
 
     EditScreen(){
@@ -37,12 +48,48 @@ public class EditScreen implements Screen {
         batch = new SpriteBatch();
         stage = new Stage(viewport, batch);
 
+        texture2 = new Texture(Gdx.files.internal("scout_is_scared.png"));
+        Image targetZone = new Image(texture2);
+        sprite2 = new Sprite(texture2);
+
         texture = new Texture(Gdx.files.internal("medic.png"));
         sprite = new Sprite(texture);
+        Image draggableItem = new Image(texture);
 
-        sprite.setPosition(WORLD_WIDTH / 2f - sprite.getWidth() / 2, WORLD_HEIGHT / 2f - sprite.getHeight() / 2);
+        draggableItem.setPosition(WORLD_WIDTH / 2f - sprite.getWidth() / 2, WORLD_HEIGHT / 2f -
+            sprite.getHeight() / 2);
+
+//        multiplexer = new InputMultiplexer();
+//
+//        multiplexer.addProcessor(stage);
+//        multiplexer.addProcessor(this);
 
         Gdx.input.setInputProcessor(stage);
+
+        stage.addActor(targetZone);
+        stage.addActor(draggableItem);
+
+        draggableItem.addListener(new DragListener(){
+            @Override
+            public void drag(InputEvent event, float x, float y, int pointer) {
+                draggableItem.moveBy(x - draggableItem.getWidth() / 2,
+                    y - draggableItem.getHeight() / 2);
+            }
+
+            @Override
+            public void dragStop(InputEvent event, float x, float y, int pointer) {
+                if (draggableItem.getX() > targetZone.getX() &&
+                    draggableItem.getX() < targetZone.getX() + targetZone.getWidth() &&
+                    draggableItem.getY() > targetZone.getY() &&
+                    draggableItem.getY() < targetZone.getY() + targetZone.getHeight())
+                {
+                    Gdx.app.log("DragAndDrop", "Item dropped in target zone");
+                } else {
+                    draggableItem.setPosition(0, 0);
+                    Gdx.app.log("DragAndDrop", "Sosav?");
+                }
+            }
+        });
     }
 
     @Override
@@ -50,6 +97,7 @@ public class EditScreen implements Screen {
         batch.dispose();
         stage.dispose();
         texture.dispose();
+        texture2.dispose();
 
     }
 
@@ -60,15 +108,15 @@ public class EditScreen implements Screen {
 
     @Override
     public void render(float delta) {
-        ScreenUtils.clear(0.15f, 0.15f, 0.2f, 1f);
+//        ScreenUtils.clear(0.15f, 0.15f, 0.2f, 1f);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
         camera.update();
 
         batch.setProjectionMatrix(camera.combined);
 
-        batch.begin();
-        sprite.draw(batch);
-        batch.end();
+        stage.act(delta);
+        stage.draw();
     }
 
     @Override
@@ -93,4 +141,49 @@ public class EditScreen implements Screen {
 
     }
 
+    @Override
+    public boolean keyDown(int keycode) {
+        return false;
+    }
+
+    @Override
+    public boolean keyUp(int keycode) {
+        return false;
+    }
+
+    @Override
+    public boolean keyTyped(char character) {
+        return false;
+    }
+
+    @Override
+    public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+        System.out.println("Sosav?");
+        return true;
+    }
+
+    @Override
+    public boolean touchUp(int screenX, int screenY, int pointer, int button) {
+        return false;
+    }
+
+    @Override
+    public boolean touchCancelled(int screenX, int screenY, int pointer, int button) {
+        return false;
+    }
+
+    @Override
+    public boolean touchDragged(int screenX, int screenY, int pointer) {
+        return false;
+    }
+
+    @Override
+    public boolean mouseMoved(int screenX, int screenY) {
+        return false;
+    }
+
+    @Override
+    public boolean scrolled(float amountX, float amountY) {
+        return false;
+    }
 }
---
+++ core/src/main/java/io/github/animation_tree/TestScreen.java
@@ -0,0 +1,45 @@
+package io.github.animation_tree;
+
+import com.badlogic.gdx.Screen;
+
+public class TestScreen implements Screen {
+
+    TestScreen(){
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void render(float delta) {
+
+    }
+
+    @Override
+    public void show() {
+
+    }
+}
